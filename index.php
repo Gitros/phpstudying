8495786b3e78a162cb3f29b5e95c722865ffb668
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-25 19:20:33 +0200

    clear

--- index.php
+++ index.php
@@ -8,39 +8,7 @@
 </head>
 <body>
     <?php 
-        echo 5.7 * 9;
-        echo "<br>";
-        echo 10 % 3;
-        echo "<br>";
-        echo 4 + 5 * 10;
-        echo "<br>";
-        echo (4 + 5) * 10;
-        echo "<br>";
-        $num = 10;
-        echo $num;
-        echo "<br>";
-        $num++;
-        echo $num;
-        echo "<br>";
-        $num += 25;
-        echo $num;
-        echo "<br>";
-        echo abs(-100);
-        echo "<br>";
-        echo pow(2,4);
-        echo "<br>";
-        echo sqrt(144);
-        echo "<br>";
-        echo max(2, 10);
-        echo "<br>";
-        echo min(2, 10);
-        echo "<br>";
-        echo round(3.2);
-        echo "<br>";
-        echo ceil(3.3);
-        echo "<br>";
-        echo floor(3.3);
-        echo "<br>";
+
     ?>    
 </body>
 </html>
