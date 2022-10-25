89199f18bd16b50b5cba8c497eefaf75e56b06d9
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-25 18:36:37 +0200

    variables

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "There once was a man named John <br>";
+        echo "He was 70years old <br>";
+        echo "He really liked the name John <br>";
+        echo "But didn't like being 35 <br>";
+    
+    ?>    
+
+
+</body>
+</html>
