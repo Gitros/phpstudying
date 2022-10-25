5c39bf025efb7bec42425af3f7b2b2ab4d1cd795
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-25 19:17:57 +0200

    math

--- index.php
+++ index.php
@@ -8,7 +8,24 @@
 </head>
 <body>
     <?php 
-
+        echo 5.7 * 9;
+        echo "<br>";
+        echo 10 % 3;
+        echo "<br>";
+        echo 4 + 5 * 10;
+        echo "<br>";
+        echo (4 + 5) * 10;
+        echo "<br>";
+        $num = 10;
+        echo $num;
+        echo "<br>";
+        $num++;
+        echo $num;
+        echo "<br>";
+        $num += 25;
+        echo $num;
+        echo "<br>";
+        
     ?>    
 </body>
 </html>
