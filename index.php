d5384494af1c3c40f482f476ab717f369acb2cd6
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-25 19:27:13 +0200

    data from input

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
 
+    <form action="index.php" method="get">
+        Name: <input type="text" name="name">
+        <input type="submit">
+    </form>
+    <br>
+    <?php 
+        echo $_GET["name"];
     ?>    
 </body>
 </html>
