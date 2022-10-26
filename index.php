a644cd5407e2659979ff22f6fd832b5007f387a8
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-26 22:41:48 +0200

    Mad libs game pt2

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 <body>
 
     <form action="index.php" method="get">
+        Color: <input type="text" name="color">
+        <br>
+        Plural Noun: <input type="text" name="pluralNoun">
+        <br>
+        Celebrity: <input type="text" name="celebrity">
+        <br>
+
         <input type="submit">
     </form>
 
@@ -16,9 +23,13 @@
     <br>
     
     <?php 
-       echo "Roses are red <br>";
-       echo "Violets are blue <br>";
-       echo "I love you <br>";
+        $color = $_GET["color"];
+        $pluralNoun = $_GET["pluralNoun"];
+        $celebrity = $_GET["celebrity"];
+
+       echo "Roses are $color <br>";
+       echo "$pluralNoun are blue <br>";
+       echo "I love $celebrity <br>";
     ?>    
 </body>
 </html>
