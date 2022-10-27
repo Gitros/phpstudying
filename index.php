5596dea60f60607a120d8ba77be2cec12f74c97a
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-27 19:43:38 +0200

    checkboxes

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
 </head>
 <body>
     <form action="index.php" method="POST">
+       Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+       Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+       Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
         <input type="submit">
     </form>
 
     <?php
-
+        $fruits = $_POST["fruits"];
+        echo $fruits[2];
     ?>    
 
 </body>
