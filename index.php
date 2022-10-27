228b4785ff2b5c4967dc5a8db508b322cee1a3a7
Gitros <31899790+Gitros@users.noreply.github.com>
2022-10-27 21:36:36 +0200

    clear

--- index.php
+++ index.php
@@ -9,18 +9,7 @@
 <body>
 
     <?php
-        $isMale = false;
-        $isTall = true;
-        if ($isMale && $isTall) {
-            echo "You are a tall male";
-        } elseif($isMale && !$isTall) {
-            echo "you are male but not tall";
-        } elseif(!$isMale && $isTall) {
-            echo "you are not male but tall";
-        } 
-        else {
-            echo "You are not male";
-        }
+
     ?>    
 
 </body>
