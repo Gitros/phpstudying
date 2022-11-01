ee85cbb63a2adac8df1eb654232ef8862d744da5
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-01 17:23:17 +0100

    object functions

--- index.php
+++ index.php
@@ -9,7 +9,20 @@
 <body>
 
     <?php
+        class Student {
+            var $name;
+            var $major;
+            var $gpa;
 
+            function __construct($name, $major, $gpa){
+                $this -> name = $name;
+                $this -> major = $major;
+                $this -> gpa = $gpa;
+            }
+        }
+
+        $student1 = new Student("Jim", "Business", 2.8);
+        $student2 = new Student("Pam", "Art", 3.6);
     ?>
 
 </body>
