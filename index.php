633b397d044bcc0b47e4b6eb656b67fe86d1e60d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-01 16:34:18 +0100

    clear

--- index.php
+++ index.php
@@ -8,34 +8,6 @@
 </head>
 <body>
 
-    <form action="index.php" method="POST">
-        First Num: <input type="number" step="0.1" name="num1"> <br>
-        OP: <input type="text" name="op"> <br>
-        Second Num: <input type="number" name="num2"> <br>
-        <input type="submit">
-    </form>
-
-
-    <?php
-        $num1 = $_POST["num1"];
-        $num2 = $_POST["num2"];
-        $op = $_POST["op"];
-
-        if($op == "+"){
-            echo $num1 + $num2;
-        } else if ($op == "-"){
-            echo $num1 - $num2;
-        }
-        else if ($op == "/"){
-            echo $num1 / $num2;
-        }
-        else if ($op == "*"){
-            echo $num1 * $num2;
-        } else {
-            echo "invalid operator";
-        }
-
-    ?>    
 
 </body>
 </html>
