f7ab524ac3663b7c6ed83278ef9fd4b00feabee9
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-01 16:21:28 +0100

    clear

--- index.php
+++ index.php
@@ -9,19 +9,7 @@
 <body>
 
     <?php
-        function getMax($num1, $num2, $num3){
-            if($num1 >= $num2 && $num1 >= $num3){
-                return $num1;
-            }
-            elseif($num2 >= $num1 && $num2 >= $num3) {
-                return $num2;
-            }
-            else{
-                return $num3;
-            }
-        };
 
-        echo getMax(300,90,400);
     ?>    
 
 </body>
