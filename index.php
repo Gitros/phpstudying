29fe328777d7a2025bc6f63c2f5c7a12be536ab7
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-01 17:13:09 +0100

    classes and objects

--- index.php
+++ index.php
@@ -9,7 +9,26 @@
 <body>
 
     <?php
-    
+        class Book{
+            var $title;
+            var $author;
+            var $pages;
+        } 
+
+        $book1 = new Book;
+        $book1 -> title = "Harry Potter";
+        $book1 -> author = "Jk Rowling";
+        $book1 -> pages = "500";
+
+        $book2 = new Book;
+        $book2 -> title = "lord of the rings";
+        $book2 -> author = "tolkien";
+        $book2 -> pages = "500";
+
+        
+
+        echo $book1 -> title;
+
     ?>
 
 </body>
